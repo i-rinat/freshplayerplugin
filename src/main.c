d98413d1b42aac455b93f3b0ed5d185c237cea9c
Rinat <ibragimovrinat@mail.ru>
2013-09-30 19:38:28 +0400

    initialize instance

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <ppapi/c/ppb.h>
 #include <ppapi/c/pp_module.h>
+#include <ppapi/c/ppp_instance.h>
 #include "interface_list.h"
 #include "trace.h"
 
@@ -132,6 +133,18 @@ main(void)
 
     int32_t res = ppp_initialize_module(42, browser_get_interface);
     trace_info("res = %d\n", res);
+    struct PPP_Instance_1_1 *ppp_instance_1_1 = ppp_get_interface(PPP_INSTANCE_INTERFACE_1_1);
+    trace_info("p = %p\n", ppp_instance_1_1);
+    trace_info("DidCreate = %p\n", ppp_instance_1_1->DidCreate);
+    trace_info("DidDestroy = %p\n", ppp_instance_1_1->DidDestroy);
+    trace_info("DidChangeView = %p\n", ppp_instance_1_1->DidChangeView);
+    trace_info("DidChangeFocus = %p\n", ppp_instance_1_1->DidChangeFocus);
+    trace_info("HandleDocumentLoad = %p\n", ppp_instance_1_1->HandleDocumentLoad);
+
+    char *argn[] = {"id"};
+    char *argv[] = {"hello"};
+    ppp_instance_1_1->DidCreate(1, 1, argn, argv);
+
 
     return 0;
 }
