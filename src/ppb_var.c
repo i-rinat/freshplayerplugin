419f4453fb25d5a4341913d8731f53e83919d3cf
Rinat <ibragimovrinat@mail.ru>
2013-10-01 23:56:48 +0400

    stubs: ppb_var.c

--- src/ppb_var.c
+++ src/ppb_var.c
@@ -1,10 +1,43 @@
 #include <ppapi/c/ppb_var.h>
 #include <stddef.h>
+#include "trace.h"
+
+static
+void
+ppb_var_add_ref(struct PP_Var var)
+{
+    trace_info("{zilch} %s\n", __func__);
+}
+
+static
+void
+ppb_var_release(struct PP_Var var)
+{
+    trace_info("{zilch} %s\n", __func__);
+}
+
+static
+struct PP_Var
+ppb_var_var_from_utf8(const char* data, uint32_t len)
+{
+    trace_info("{zilch} %s\n        data = %.*s, len = %d\n", __func__, len, data, len);
+    struct PP_Var var = {0};
+    return var;
+}
+
+static
+const char *
+ppb_var_var_to_utf8(struct PP_Var var, uint32_t *len)
+{
+    trace_info("{zilch} %s\n", __func__);
+    *len = 5;
+    return "zilch";
+}
 
 
 const struct PPB_Var_1_1 ppb_var_interface_1_1 = {
-    .AddRef = NULL,
-    .Release = NULL,
-    .VarFromUtf8 = NULL,
-    .VarToUtf8 = NULL
+    .AddRef = ppb_var_add_ref,
+    .Release = ppb_var_release,
+    .VarFromUtf8 = ppb_var_var_from_utf8,
+    .VarToUtf8 = ppb_var_var_to_utf8
 };
