3dca0e5137ab800ca2adba9a71ea811edd47902c
Rinat <ibragimovrinat@mail.ru>
2013-10-20 00:58:13 +0400

    update trace code in PPB_Var

--- src/ppb_var.c
+++ src/ppb_var.c
@@ -25,6 +25,39 @@ destructor_ppb_var(void)
     g_hash_table_unref(var_ht);
 }
 
+static
+void
+trace_var(const char *impl_status, const char *func, struct PP_Var var)
+{
+    switch (var.type) {
+    case PP_VARTYPE_UNDEFINED:
+        trace_info("[PPB] %s %s var = {.type=UNDEFINED}\n", impl_status, func);
+        break;
+    case PP_VARTYPE_NULL:
+        trace_info("[PPB] %s %s var = {.type=NULL}\n", impl_status, func);
+        break;
+    case PP_VARTYPE_BOOL:
+        trace_info("[PPB] %s %s var = {.type=BOOL, .value.as_int=%d}\n",
+                   impl_status, func, var.value.as_int);
+        break;
+    case PP_VARTYPE_INT32:
+        trace_info("[PPB] %s %s var = {.type=INT32, .value.as_int=%d}\n",
+                   impl_status, func, var.value.as_int);
+    case PP_VARTYPE_DOUBLE:
+        trace_info("[PPB] %s %s var = {.type=DOUBLE, .value.as_double=%f}\n",
+                   impl_status, func, var.value.as_double);
+        break;
+    case PP_VARTYPE_STRING:
+        trace_info("[PPB] %s %s var = {.type=STRING, .value.as_id(str)=%s}\n",
+                   impl_status, func, (char *)(size_t)var.value.as_id);
+        break;
+    default:
+        trace_info("[PPB] %s %s var = {.type=NOTIMPLEMENTED (%d)}\n",
+                   impl_status, func, var.type);
+        break;
+    }
+}
+
 int
 ref_var(struct PP_Var var)
 {
@@ -45,18 +78,11 @@ static
 void
 ppb_var_add_ref(struct PP_Var var)
 {
-    trace_info("{full} %s\n        var = {.type = %d, .value.as_int = %d, .value.as_double = %f, "
-               ".value.as_id = 0x%" PRIx64 "}\n", __func__, var.type, var.value.as_int,
-               var.value.as_double, var.value.as_id);
-    switch (var.type) {
-    case PP_VARTYPE_UNDEFINED:
-    case PP_VARTYPE_NULL:
-    case PP_VARTYPE_BOOL:
-    case PP_VARTYPE_INT32:
-    case PP_VARTYPE_DOUBLE:
-        // do nothing
-        return;
-    default:
+    trace_var("{full}", __func__, var);
+    if (var.type == PP_VARTYPE_STRING || var.type == PP_VARTYPE_OBJECT ||
+        var.type == PP_VARTYPE_ARRAY || var.type == PP_VARTYPE_DICTIONARY ||
+        var.type == PP_VARTYPE_ARRAY_BUFFER)
+    {
         ref_var(var);
     }
 }
@@ -65,19 +91,11 @@ static
 void
 ppb_var_release(struct PP_Var var)
 {
-    trace_info("{full} %s\n        var = {.type = %d, .value.as_int = %d, .value.as_double = %f, "
-               ".value.as_id = 0x%" PRIx64 "}\n", __func__, var.type, var.value.as_int,
-               var.value.as_double, var.value.as_id);
-
-    switch (var.type) {
-    case PP_VARTYPE_UNDEFINED:
-    case PP_VARTYPE_NULL:
-    case PP_VARTYPE_BOOL:
-    case PP_VARTYPE_INT32:
-    case PP_VARTYPE_DOUBLE:
-        // do nothing
-        return;
-    default:
+    trace_var("{full}", __func__, var);
+    if (var.type == PP_VARTYPE_STRING || var.type == PP_VARTYPE_OBJECT ||
+        var.type == PP_VARTYPE_ARRAY || var.type == PP_VARTYPE_DICTIONARY ||
+        var.type == PP_VARTYPE_ARRAY_BUFFER)
+    {
         if (unref_var(var) == 0 && var.type == PP_VARTYPE_STRING)
             free((void*)(size_t)var.value.as_id);
     }
@@ -87,7 +105,7 @@ static
 struct PP_Var
 ppb_var_var_from_utf8_1_1(const char *data, uint32_t len)
 {
-    trace_info("{full} %s\n        data = %.*s, len = %d\n", __func__, len, data, len);
+    trace_info("{full} %s data=%.*s, len=%d\n", __func__, len, data, len);
     struct PP_Var var = {0};
     char *cpy = malloc(len + 1);
 
@@ -103,8 +121,7 @@ static
 struct PP_Var
 ppb_var_var_from_utf8_1_0(PP_Module module, const char *data, uint32_t len)
 {
-    trace_info("{full} %s\n        module = %d, data = %.*s, len = %d\n", __func__,
-               module, len, data, len);
+    trace_info("{full} %s module=%d, data=%.*s, len=%d\n", __func__, module, len, data, len);
     struct PP_Var var = {0};
     char *cpy = malloc(len + 1);
 
@@ -121,9 +138,7 @@ static
 const char *
 ppb_var_var_to_utf8(struct PP_Var var, uint32_t *len)
 {
-    trace_info("{full} %s\n        var = {.type = %d, .value.as_int = %d, .value.as_double = %f, "
-               ".value.as_id = 0x%" PRIx64 "}\n", __func__, var.type, var.value.as_int,
-               var.value.as_double, var.value.as_id);
+    trace_var("{full}", __func__, var);
     if (PP_VARTYPE_STRING == var.type) {
         const char *s = (void *)(size_t)var.value.as_id;
         *len = strlen(s);
