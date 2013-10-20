ade60ded87fc7faa615d79f1666ad08ef62fcb75
Rinat <ibragimovrinat@mail.ru>
2013-10-20 22:46:23 +0400

    parse urls in ppb_url_util_dev.c

--- src/ppb_url_util_dev.c
+++ src/ppb_url_util_dev.c
@@ -1,6 +1,7 @@
 #include <ppapi/c/dev/ppb_url_util_dev.h>
 #include <stddef.h>
 #include <string.h>
+#include <Uri.h>
 #include "trace.h"
 #include "utils.h"
 
@@ -59,6 +60,69 @@ ppb_url_util_dev_document_can_access_document(PP_Instance active, PP_Instance ta
 }
 
 static
+void
+parse_url_string(const char *s, struct PP_URLComponents_Dev *components)
+{
+    UriParserStateA ups;
+    UriUriA uri;
+
+    ups.uri = &uri;
+    if (uriParseUriA(&ups, s) != URI_SUCCESS) {
+        uriFreeUriMembersA(&uri);
+        trace_warning("failure at %s\n", __func__);
+        return;
+    }
+
+#ifdef C_PARSE
+#error conflicting defininition of C_PARSE
+#endif
+
+#define C_PARSE(c1, c2) \
+    components->c1.begin = uri.c2.first ? uri.c2.first - s + 1 : 0; \
+    components->c1.len = uri.c2.first ? uri.c2.afterLast - uri.c2.first : -1;
+
+    C_PARSE(scheme, scheme);
+    C_PARSE(username, userInfo);
+    C_PARSE(password, userInfo);
+    if (components->username.begin > 0) {
+        const char *first = s + components->username.begin - 1;
+        const char *last = first + components->username.len;
+        const char *ptr = first;
+        while (ptr < last) {
+            if (*ptr == ':') {
+                components->username.len = ptr - first;
+                components->password.begin += ptr - first + 1;
+                components->password.len -= ptr - first + 1;
+                if (components->username.len == 0) {
+                    components->username.begin = 0;
+                    components->username.len = -1;
+                }
+                if (components->password.len == 0) {
+                    components->password.begin = 0;
+                    components->password.len = -1;
+                }
+                break;
+            }
+            ptr ++;
+        }
+    }
+    C_PARSE(host, hostText);
+    C_PARSE(port, portText);
+    components->path.begin = uri.pathHead ? uri.pathHead->text.first - s + 1 : 0;
+    components->path.len = uri.pathHead ? uri.pathTail->text.afterLast - uri.pathHead->text.first : -1;
+    if (components->path.begin > 0) {
+        components->path.begin -= 1;
+        components->path.len += 1;
+    }
+    C_PARSE(query, query);
+    C_PARSE(ref, fragment);
+
+#undef C_PARSE
+
+    uriFreeUriMembersA(&uri);
+}
+
+static
 struct PP_Var
 ppb_url_util_dev_get_document_url(PP_Instance instance, struct PP_URLComponents_Dev *components)
 {
@@ -68,22 +132,7 @@ ppb_url_util_dev_get_document_url(PP_Instance instance, struct PP_URLComponents_
     var.value.as_id = (int64_t)(void*)strdup("http://127.0.0.1/flashtest/");
     ref_var(var);
     if (components) {
-        components->scheme.begin = 1;
-        components->scheme.len = 4;
-        components->username.begin = 0;
-        components->username.len = -1;
-        components->password.begin = 0;
-        components->password.len = -1;
-        components->host.begin = 8;
-        components->host.len = 9;
-        components->port.begin = 0;
-        components->port.len = -1;
-        components->path.begin = 17;
-        components->path.len = 11;
-        components->query.begin = 0;
-        components->query.len = -1;
-        components->ref.begin = 0;
-        components->ref.len = -1;
+        parse_url_string((void*)(size_t)var.value.as_id, components);
     }
 
     return var;
@@ -100,22 +149,7 @@ ppb_url_util_dev_get_plugin_instance_url(PP_Instance instance,
     var.value.as_id = (int64_t)(void*)strdup("http://127.0.0.1/flashtest/flowplayer-3.2.16.swf");
     ref_var(var);
     if (components) {
-        components->scheme.begin = 1;
-        components->scheme.len = 4;
-        components->username.begin = 0;
-        components->username.len = -1;
-        components->password.begin = 0;
-        components->password.len = -1;
-        components->host.begin = 8;
-        components->host.len = 9;
-        components->port.begin = 0;
-        components->port.len = -1;
-        components->path.begin = 17;
-        components->path.len = 32;
-        components->query.begin = 0;
-        components->query.len = -1;
-        components->ref.begin = 0;
-        components->ref.len = -1;
+        parse_url_string((void*)(size_t)var.value.as_id, components);
     }
 
     return var;
