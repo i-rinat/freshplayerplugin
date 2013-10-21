aa5d6b4278f7d18c8e64f95f7a451a461804ffaf
Rinat <ibragimovrinat@mail.ru>
2013-10-21 22:31:24 +0400

    stubs: ppb_flash_fullscreen.c

--- src/ppb_flash_fullscreen.c
+++ src/ppb_flash_fullscreen.c
@@ -1,9 +1,35 @@
 #include <ppapi/c/private/ppb_flash_fullscreen.h>
 #include <stddef.h>
+#include "trace.h"
+
+
+static
+PP_Bool
+ppb_flash_fullscreen_is_fullscreen(PP_Instance instance)
+{
+    trace_info("[PPB] {zilch} %s instance=%d\n", __func__, instance);
+    return PP_TRUE;
+}
+
+static
+PP_Bool
+ppb_flash_fullscreen_set_fullscreen(PP_Instance instance, PP_Bool fullscreen)
+{
+    trace_info("[PPB] {zilch} %s instance=%d fullscreen=%d\n", __func__, instance, fullscreen);
+    return PP_TRUE;
+}
+
+static
+PP_Bool
+ppb_flash_fullscreen_get_screen_size(PP_Instance instance, struct PP_Size* size)
+{
+    trace_info("[PPB] {zilch} %s instance=%d\n", __func__, instance);
+    return PP_TRUE;
+}
 
 
 const struct PPB_FlashFullscreen_1_0 ppb_flash_fullscreen_interface_1_0 = {
-    .IsFullscreen = (void *)244,
-    .SetFullscreen = (void *)245,
-    .GetScreenSize = (void *)246,
+    .IsFullscreen =     ppb_flash_fullscreen_is_fullscreen,
+    .SetFullscreen =    ppb_flash_fullscreen_set_fullscreen,
+    .GetScreenSize =    ppb_flash_fullscreen_get_screen_size,
 };
