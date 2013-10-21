b5381e442e15a5e9f8e424a4014091214251c1a1
Rinat <ibragimovrinat@mail.ru>
2013-10-21 22:25:39 +0400

    stubs: ppb_url_loader_trusted.c

--- src/ppb_url_loader_trusted.c
+++ src/ppb_url_loader_trusted.c
@@ -1,8 +1,24 @@
 #include <ppapi/c/trusted/ppb_url_loader_trusted.h>
 #include <stddef.h>
+#include "trace.h"
 
+
+static
+void
+ppb_url_loader_trusted_grant_universal_access(PP_Resource loader)
+{
+    trace_info("[PPB] {zilch} %s loader=%d\n", __func__, loader);
+}
+
+static
+void
+ppb_url_loader_trusted_register_status_callback(PP_Resource loader,
+                                                PP_URLLoaderTrusted_StatusCallback cb)
+{
+    trace_info("[PPB] {zilch} %s loader=%d, cb=%p\n", __func__, loader, cb);
+}
 
 const struct PPB_URLLoaderTrusted_0_3 ppb_url_loader_trusted_interface_0_3 = {
-    .GrantUniversalAccess = (void *)15,
-    .RegisterStatusCallback = (void *)16,
+    .GrantUniversalAccess =     ppb_url_loader_trusted_grant_universal_access,
+    .RegisterStatusCallback =   ppb_url_loader_trusted_register_status_callback,
 };
