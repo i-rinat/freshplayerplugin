9507062d5c62cf6eb0dba65fe383aeb5dde556ca
Rinat <ibragimovrinat@mail.ru>
2013-10-21 22:25:13 +0400

    stubs: ppb_url_response_info.c

--- src/ppb_url_response_info.c
+++ src/ppb_url_response_info.c
@@ -1,9 +1,36 @@
 #include <ppapi/c/ppb_url_response_info.h>
 #include <stddef.h>
+#include "trace.h"
+
+
+static
+PP_Bool
+ppb_url_response_info_is_url_response_info(PP_Resource resource)
+{
+    trace_info("[PPB] {zilch} %s resource=%d\n", __func__, resource);
+    return PP_TRUE;
+}
+
+static
+struct PP_Var
+ppb_url_response_info_get_property(PP_Resource response, PP_URLResponseProperty property)
+{
+    struct PP_Var var = {0};
+    trace_info("[PPB] {zilch} %s response=%d, property=%d\n", __func__, response, property);
+    return var;
+}
+
+static
+PP_Resource
+ppb_url_response_info_get_body_as_file_ref(PP_Resource response)
+{
+    trace_info("[PPB] {zilch} %s response=%d\n", __func__, response);
+    return 0;
+}
 
 
 const struct PPB_URLResponseInfo_1_0 ppb_url_response_info_interface_1_0 = {
-    .IsURLResponseInfo = (void *)7,
-    .GetProperty = (void *)8,
-    .GetBodyAsFileRef = (void *)9,
+    .IsURLResponseInfo =    ppb_url_response_info_is_url_response_info,
+    .GetProperty =          ppb_url_response_info_get_property,
+    .GetBodyAsFileRef =     ppb_url_response_info_get_body_as_file_ref,
 };
