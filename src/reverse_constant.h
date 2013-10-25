704595ed59d217407b1a88822e9a782dcf22d039
Rinat <ibragimovrinat@mail.ru>
2013-10-26 00:51:09 +0400

    +reverse_pp_url_request_property

--- src/reverse_constant.h
+++ src/reverse_constant.h
@@ -3,9 +3,11 @@
 
 #include <npapi.h>
 #include <ppapi/c/ppb_url_response_info.h>
+#include <ppapi/c/ppb_url_request_info.h>
 
 const char *    reverse_npp_variable(NPPVariable var);
 const char *    reverse_npn_variable(NPNVariable var);
 const char *    reverse_pp_url_response_property(PP_URLResponseProperty prop);
+const char *reverse_pp_url_request_property(PP_URLRequestProperty prop);
 
 #endif // FPP__REVERSE_CONSTANT_H
