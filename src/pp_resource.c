05e911d6d757c27a2f0b1b13c5ec1d6c2f68d138
Rinat <ibragimovrinat@mail.ru>
2013-10-25 23:05:26 +0400

    add pp_resource, resource table storage

---
+++ src/pp_resource.c
@@ -0,0 +1,100 @@
+#include "pp_resource.h"
+#include <glib.h>
+#include <stdlib.h>
+
+
+static GArray          *res_tbl;
+static pthread_mutex_t  res_tbl_lock = PTHREAD_MUTEX_INITIALIZER;
+
+static
+__attribute__((constructor))
+void
+pp_resource_constructor(void)
+{
+    void *null_val = NULL;
+    res_tbl = g_array_new(FALSE, FALSE, sizeof(void*));
+    g_array_append_val(res_tbl, null_val);
+}
+
+PP_Resource
+pp_resource_allocate(enum pp_resource_type_e type)
+{
+    void *ptr;
+
+#define ALLOC_HELPER(typename)              \
+    ptr = calloc(sizeof(typename), 1);      \
+    ((typename *)ptr)->_parent.type = type;
+
+    pthread_mutex_lock(&res_tbl_lock);
+    switch (type) {
+    case PP_RESOURCE_URL_LOADER:
+        ALLOC_HELPER(struct pp_url_loader_resource_s);
+        break;
+    default:
+        // fall through
+    case PP_RESOURCE_UNKNOWN:
+        ptr = calloc(sizeof(struct pp_resource_generic_s), 1);
+        ((struct pp_resource_generic_s *)ptr)->type = type;
+        break;
+    }
+
+    int handle = res_tbl->len;
+    g_array_append_val(res_tbl, ptr);
+
+    pthread_mutex_unlock(&res_tbl_lock);
+    return handle;
+}
+
+void
+pp_resource_expunge(PP_Resource resource)
+{
+    pthread_mutex_lock(&res_tbl_lock);
+    if (resource < 1 || resource >= res_tbl->len) {
+        pthread_mutex_unlock(&res_tbl_lock);
+        return;
+    }
+
+    void **ptr = &g_array_index(res_tbl, void *, resource);
+    free(*ptr);
+    *ptr = NULL;
+    pthread_mutex_unlock(&res_tbl_lock);
+}
+
+void *
+pp_resource_acquire(PP_Resource resource)
+{
+    pthread_mutex_lock(&res_tbl_lock);
+    if (resource < 1 || resource >= res_tbl->len) {
+        pthread_mutex_unlock(&res_tbl_lock);
+        return NULL;
+    }
+
+    // TODO: add mutexes for every resource
+    // TODO: lock mutex of particular resource handle
+    struct pp_resource_generic_s *ptr = g_array_index(res_tbl, void*, resource);
+
+    pthread_mutex_unlock(&res_tbl_lock);
+    return ptr;
+}
+
+void
+pp_resource_release(PP_Resource resource)
+{
+    // TODO: unlock mutex of particular resource handle
+}
+
+enum pp_resource_type_e
+pp_resource_get_type(PP_Resource resource)
+{
+    pthread_mutex_lock(&res_tbl_lock);
+    if (resource < 1 || resource >= res_tbl->len) {
+        pthread_mutex_unlock(&res_tbl_lock);
+        return PP_RESOURCE_UNKNOWN;
+    }
+
+    struct pp_resource_generic_s *ptr = g_array_index(res_tbl, void*, resource);
+    enum pp_resource_type_e type = ptr->type;
+
+    pthread_mutex_unlock(&res_tbl_lock);
+    return type;
+}
---
+++ src/pp_resource.h
@@ -0,0 +1,28 @@
+#ifndef FFP__PP_RESOURCE_H
+#define FFP__PP_RESOURCE_H
+
+#include <ppapi/c/pp_resource.h>
+
+enum pp_resource_type_e {
+    PP_RESOURCE_UNKNOWN = 0,
+    PP_RESOURCE_URL_LOADER,
+};
+
+
+struct pp_resource_generic_s {
+    int         type;
+};
+
+struct pp_url_loader_resource_s {
+    struct pp_resource_generic_s _parent;
+
+};
+
+PP_Resource             pp_resource_allocate(enum pp_resource_type_e type);
+void                    pp_resource_expunge(PP_Resource resource);
+void                   *pp_resource_acquire(PP_Resource resource);
+void                    pp_resource_release(PP_Resource resource);
+enum pp_resource_type_e pp_resource_get_type(PP_Resource resource);
+
+
+#endif // FFP__PP_RESOURCE_H
