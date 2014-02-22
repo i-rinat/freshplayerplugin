552690ba759d8c030613d38a437640bf5bdab4cd
Rinat <ibragimovrinat@mail.ru>
2014-02-22 17:09:32 +0400

    add stub for PPB_OpenGLES2FramebufferBlit interface

--- src/interface_list.h
+++ src/interface_list.h
@@ -103,6 +103,7 @@ extern const struct PPB_NetAddress_Private_1_1      ppb_net_address_private_inte
 extern const struct PPB_NetworkMonitor_1_0          ppb_network_monitor_interface_1_0;
 extern const struct PPB_OpenGLES2ChromiumEnableFeature ppb_opengles2_chromium_enable_feature_interface_1_0;
 extern const struct PPB_OpenGLES2ChromiumMapSub     ppb_opengles2_chromium_map_sub_interface_1_0;
+extern const struct PPB_OpenGLES2FramebufferBlit    ppb_opengles2_framebuffer_blit_interface_1_0;
 extern const struct PPB_OpenGLES2InstancedArrays    ppb_opengles2_instanced_arrays_interface_1_0;
 extern const struct PPB_OpenGLES2                   ppb_opengles2_interface_1_0;
 extern const struct PPB_Printing_Dev_0_7            ppb_printing_dev_0_7;
