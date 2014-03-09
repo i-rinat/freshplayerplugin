d660d6d011768dd4d12b7fe9e13ca543e92a2cf3
Rinat <ibragimovrinat@mail.ru>
2014-03-09 16:11:33 +0400

    add stub for ppb_udp_socket_private

--- src/ppb_udp_socket_private.c
+++ src/ppb_udp_socket_private.c
@@ -23,17 +23,160 @@
  */
 
 #include "ppb_udp_socket_private.h"
-#include <stddef.h>
+#include <stdlib.h>
+#include "trace.h"
+
+
+PP_Resource
+ppb_udp_socket_private_create(PP_Instance instance_id)
+{
+    return 0;
+}
+
+PP_Bool
+ppb_udp_socket_private_is_udp_socket(PP_Resource resource_id)
+{
+    return PP_TRUE;
+}
+
+int32_t
+ppb_udp_socket_private_set_socket_feature(PP_Resource udp_socket, PP_UDPSocketFeature_Private name,
+                                          struct PP_Var value)
+{
+    return -1;
+}
+
+int32_t
+ppb_udp_socket_private_bind(PP_Resource udp_socket, const struct PP_NetAddress_Private *addr,
+                            struct PP_CompletionCallback callback)
+{
+    return -1;
+}
+
+PP_Bool
+ppb_udp_socket_private_get_bound_address(PP_Resource udp_socket, struct PP_NetAddress_Private *addr)
+{
+    return PP_FALSE;
+}
+
+int32_t
+ppb_udp_socket_private_recv_from(PP_Resource udp_socket, char *buffer, int32_t num_bytes,
+                                 struct PP_CompletionCallback callback)
+{
+    return -1;
+}
+
+PP_Bool
+ppb_udp_socket_private_get_recv_from_address(PP_Resource udp_socket,
+                                             struct PP_NetAddress_Private *addr)
+{
+    return PP_FALSE;
+}
+
+int32_t
+ppb_udp_socket_private_send_to(PP_Resource udp_socket, const char *buffer, int32_t num_bytes,
+                               const struct PP_NetAddress_Private *addr,
+                               struct PP_CompletionCallback callback)
+{
+    return -1;
+}
+
+void
+ppb_udp_socket_private_close(PP_Resource udp_socket)
+{
+}
+
+
+// trace wrappers
+static
+PP_Resource
+trace_ppb_udp_socket_private_create(PP_Instance instance_id)
+{
+    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    return ppb_udp_socket_private_create(instance_id);
+}
+
+static
+PP_Bool
+trace_ppb_udp_socket_private_is_udp_socket(PP_Resource resource_id)
+{
+    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    return ppb_udp_socket_private_is_udp_socket(resource_id);
+}
+
+static
+int32_t
+trace_ppb_udp_socket_private_set_socket_feature(PP_Resource udp_socket,
+                                                PP_UDPSocketFeature_Private name,
+                                                struct PP_Var value)
+{
+    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    return ppb_udp_socket_private_set_socket_feature(udp_socket, name, value);
+}
+
+static
+int32_t
+trace_ppb_udp_socket_private_bind(PP_Resource udp_socket, const struct PP_NetAddress_Private *addr,
+                                  struct PP_CompletionCallback callback)
+{
+    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    return ppb_udp_socket_private_bind(udp_socket, addr, callback);
+}
+
+static
+PP_Bool
+trace_ppb_udp_socket_private_get_bound_address(PP_Resource udp_socket,
+                                               struct PP_NetAddress_Private *addr)
+{
+    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    return ppb_udp_socket_private_get_bound_address(udp_socket, addr);
+}
+
+static
+int32_t
+trace_ppb_udp_socket_private_recv_from(PP_Resource udp_socket, char *buffer, int32_t num_bytes,
+                                       struct PP_CompletionCallback callback)
+{
+    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    return ppb_udp_socket_private_recv_from(udp_socket, buffer, num_bytes, callback);
+}
+
+static
+PP_Bool
+trace_ppb_udp_socket_private_get_recv_from_address(PP_Resource udp_socket,
+                                                   struct PP_NetAddress_Private *addr)
+{
+    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    return ppb_udp_socket_private_get_recv_from_address(udp_socket, addr);
+}
+
+static
+int32_t
+trace_ppb_udp_socket_private_send_to(PP_Resource udp_socket, const char *buffer, int32_t num_bytes,
+                                     const struct PP_NetAddress_Private *addr,
+                                     struct PP_CompletionCallback callback)
+{
+    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    return ppb_udp_socket_private_send_to(udp_socket, buffer, num_bytes, addr, callback);
+}
+
+static
+void
+trace_ppb_udp_socket_private_close(PP_Resource udp_socket)
+{
+    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    return ppb_udp_socket_private_close(udp_socket);
+}
 
 
 const struct PPB_UDPSocket_Private_0_4 ppb_udp_socket_private_interface_0_4 = {
-    .Create = (void *)27,
-    .IsUDPSocket = (void *)28,
-    .SetSocketFeature = (void *)29,
-    .Bind = (void *)30,
-    .GetBoundAddress = (void *)31,
-    .RecvFrom = (void *)32,
-    .GetRecvFromAddress = (void *)33,
-    .SendTo = (void *)34,
-    .Close = (void *)35,
+    .Create =               trace_ppb_udp_socket_private_create,
+    .IsUDPSocket =          trace_ppb_udp_socket_private_is_udp_socket,
+    .SetSocketFeature =     trace_ppb_udp_socket_private_set_socket_feature,
+    .Bind =                 trace_ppb_udp_socket_private_bind,
+    .GetBoundAddress =      trace_ppb_udp_socket_private_get_bound_address,
+    .RecvFrom =             trace_ppb_udp_socket_private_recv_from,
+    .GetRecvFromAddress =   trace_ppb_udp_socket_private_get_recv_from_address,
+    .SendTo =               trace_ppb_udp_socket_private_send_to,
+    .Close =                trace_ppb_udp_socket_private_close,
 };
