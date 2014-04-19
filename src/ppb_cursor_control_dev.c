b38d75c937b4a16ab520e1c2718401049aa95d06
Rinat <ibragimovrinat@mail.ru>
2014-04-19 16:54:03 +0400

    impl ppb_cursor_control_dev_set_cursor

--- src/ppb_cursor_control_dev.c
+++ src/ppb_cursor_control_dev.c
@@ -24,6 +24,10 @@
 
 #include "ppb_cursor_control_dev.h"
 #include <stdlib.h>
+#include <X11/Xlib.h>
+#include <X11/cursorfont.h>
+#include "pp_resource.h"
+#include "tables.h"
 #include "trace.h"
 
 
@@ -31,6 +35,151 @@ PP_Bool
 ppb_cursor_control_dev_set_cursor(PP_Instance instance, enum PP_CursorType_Dev type,
                                   PP_Resource custom_image, const struct PP_Point *hot_spot)
 {
+    int xtype = XC_arrow;
+    switch (type) {
+    case PP_CURSORTYPE_CUSTOM:
+        trace_warning("%s, custom cursors not implemented\n", __func__);
+        xtype = XC_arrow;
+        break;
+    case PP_CURSORTYPE_POINTER:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_CROSS:
+        xtype = XC_crosshair;
+        break;
+    case PP_CURSORTYPE_HAND:
+        xtype = XC_hand2;
+        break;
+    case PP_CURSORTYPE_IBEAM:
+        xtype = XC_xterm;
+        break;
+    case PP_CURSORTYPE_WAIT:
+        xtype = XC_watch;
+        break;
+    case PP_CURSORTYPE_HELP:
+        xtype = XC_question_arrow;
+        break;
+    case PP_CURSORTYPE_EASTRESIZE:
+        xtype = XC_right_side;
+        break;
+    case PP_CURSORTYPE_NORTHRESIZE:
+        xtype = XC_top_side;
+        break;
+    case PP_CURSORTYPE_NORTHEASTRESIZE:
+        xtype = XC_top_right_corner;
+        break;
+    case PP_CURSORTYPE_NORTHWESTRESIZE:
+        xtype = XC_top_left_corner;
+        break;
+    case PP_CURSORTYPE_SOUTHRESIZE:
+        xtype = XC_bottom_side;
+        break;
+    case PP_CURSORTYPE_SOUTHEASTRESIZE:
+        xtype = XC_bottom_right_corner;
+        break;
+    case PP_CURSORTYPE_SOUTHWESTRESIZE:
+        xtype = XC_bottom_left_corner;
+        break;
+    case PP_CURSORTYPE_WESTRESIZE:
+        xtype = XC_left_side;
+        break;
+    case PP_CURSORTYPE_NORTHSOUTHRESIZE:
+        xtype = XC_sb_v_double_arrow;
+        break;
+    case PP_CURSORTYPE_EASTWESTRESIZE:
+        xtype = XC_sb_h_double_arrow;
+        break;
+    case PP_CURSORTYPE_NORTHEASTSOUTHWESTRESIZE:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_NORTHWESTSOUTHEASTRESIZE:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_COLUMNRESIZE:
+        xtype = XC_sb_h_double_arrow;
+        break;
+    case PP_CURSORTYPE_ROWRESIZE:
+        xtype = XC_sb_v_double_arrow;
+        break;
+    case PP_CURSORTYPE_MIDDLEPANNING:
+        xtype = XC_fleur;
+        break;
+    case PP_CURSORTYPE_EASTPANNING:
+        xtype = XC_sb_right_arrow;
+        break;
+    case PP_CURSORTYPE_NORTHPANNING:
+        xtype = XC_sb_up_arrow;
+        break;
+    case PP_CURSORTYPE_NORTHEASTPANNING:
+        xtype = XC_top_right_corner;
+        break;
+    case PP_CURSORTYPE_NORTHWESTPANNING:
+        xtype = XC_top_left_corner;
+        break;
+    case PP_CURSORTYPE_SOUTHPANNING:
+        xtype = XC_sb_down_arrow;
+        break;
+    case PP_CURSORTYPE_SOUTHEASTPANNING:
+        xtype = XC_bottom_right_corner;
+        break;
+    case PP_CURSORTYPE_SOUTHWESTPANNING:
+        xtype = XC_bottom_left_corner;
+        break;
+    case PP_CURSORTYPE_WESTPANNING:
+        xtype = XC_sb_left_arrow;
+        break;
+    case PP_CURSORTYPE_MOVE:
+        xtype = XC_fleur;
+        break;
+    case PP_CURSORTYPE_VERTICALTEXT:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_CELL:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_CONTEXTMENU:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_ALIAS:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_PROGRESS:
+        xtype = XC_watch;
+        break;
+    case PP_CURSORTYPE_NODROP:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_COPY:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_NONE:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_NOTALLOWED:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_ZOOMIN:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_ZOOMOUT:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_GRAB:
+        xtype = XC_left_ptr;
+        break;
+    case PP_CURSORTYPE_GRABBING:
+        xtype = XC_left_ptr;
+        break;
+    }
+
+    struct pp_instance_s *pp_i = tables_get_pp_instance(instance);
+    Cursor cursor = XCreateFontCursor(pp_i->dpy, xtype);
+    Window wnd;
+
+    npn.getvalue(pp_i->npp, NPNVnetscapeWindow, &wnd);
+    XDefineCursor(pp_i->dpy, wnd, cursor);
+    XFlush(pp_i->dpy);
+
     return PP_TRUE;
 }
 
@@ -66,7 +215,7 @@ trace_ppb_cursor_control_dev_set_cursor(PP_Instance instance, enum PP_CursorType
                                         PP_Resource custom_image, const struct PP_Point *hot_spot)
 {
     char *s_hot_spot = trace_point_as_string(hot_spot);
-    trace_info("[PPB] {zilch} %s instance=%d, type=%d, custom_image=%d, hot_spot=%s\n", __func__+6,
+    trace_info("[PPB] {full} %s instance=%d, type=%d, custom_image=%d, hot_spot=%s\n", __func__+6,
                instance, type, custom_image, s_hot_spot);
     free(s_hot_spot);
     return ppb_cursor_control_dev_set_cursor(instance, type, custom_image, hot_spot);
