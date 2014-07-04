8b9f65354fa2e56b49ef572c822a29c2d4ce0f9b
Rinat <ibragimovrinat@mail.ru>
2014-07-05 02:16:32 +0400

    ppb_nacl: add parameter tracing

--- src/ppb_nacl_private.c
+++ src/ppb_nacl_private.c
@@ -3,6 +3,7 @@
 #include <ppapi/c/pp_errors.h>
 #include "trace.h"
 #include "tables.h"
+#include "reverse_constant.h"
 
 
 void
@@ -184,7 +185,13 @@ trace_ppb_nacl_launch_sel_ldr(PP_Instance instance, const char *alleged_url, PP_
                               void *imc_handle, struct PP_Var *error_message,
                               struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d, alleged_url=%s, uses_irt=%d, uses_ppapi=%d, "
+               "uses_nonsfi_mode=%d, enable_ppapi_dev=%d, enable_dyncode_syscalls=%d, "
+               "enable_exception_handling=%d, enable_crash_throttling=%d, imc_handle=%p, "
+               "callback={.func=%p, .user_data=%p, .flags=%u}\n", __func__+6, instance,
+               alleged_url, uses_irt, uses_ppapi, uses_nonsfi_mode, enable_ppapi_dev,
+               enable_dyncode_syscalls, enable_exception_handling, enable_crash_throttling,
+               imc_handle, callback.func, callback.user_data, callback.flags);
     return ppb_nacl_launch_sel_ldr(instance, alleged_url, uses_irt, uses_ppapi, uses_nonsfi_mode,
                                    enable_ppapi_dev, enable_dyncode_syscalls,
                                    enable_exception_handling, enable_crash_throttling, imc_handle,
@@ -195,7 +202,7 @@ TRACE_WRAPPER
 PP_ExternalPluginResult
 trace_ppb_nacl_start_ppapi_proxy(PP_Instance instance)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d\n", __func__+6, instance);
     return ppb_nacl_start_ppapi_proxy(instance);
 }
 
@@ -221,7 +228,8 @@ trace_ppb_nacl_broker_duplicate_handle(PP_FileHandle source_handle, uint32_t pro
                                        PP_FileHandle *target_handle, uint32_t desired_access,
                                        uint32_t options)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s source_handle=%d, process_id=%u, desired_access=%u, options=%u\n",
+               __func__+6, source_handle, process_id, desired_access, options);
     return ppb_nacl_broker_duplicate_handle(source_handle, process_id, target_handle,
                                             desired_access, options);
 }
@@ -230,7 +238,7 @@ TRACE_WRAPPER
 PP_FileHandle
 trace_ppb_nacl_get_readonly_pnacl_fd(const char *filename)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s filename=%s\n", __func__+6, filename);
     return ppb_nacl_get_readonly_pnacl_fd(filename);
 }
 
@@ -238,7 +246,7 @@ TRACE_WRAPPER
 PP_FileHandle
 trace_ppb_nacl_create_temporary_file(PP_Instance instance)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d\n", __func__+6, instance);
     return ppb_nacl_create_temporary_file(instance);
 }
 
@@ -266,7 +274,11 @@ trace_ppb_nacl_get_nexe_fd(PP_Instance instance, const char *pexe_url, uint32_t 
                            const char *extra_flags, PP_Bool *is_hit, PP_FileHandle *nexe_handle,
                            struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d, pexe_url=%s, abi_version=%u, opt_level=%u, "
+               "last_modified=%s, etag=%s, has_no_store_header=%d, sandbox_isa=%s, "
+               "extra_flags=%s, callback={.func=%p, .user_data=%p, .flags=%u}\n", __func__+6,
+               instance, pexe_url, abi_version, opt_level, last_modified, etag, has_no_store_header,
+               sandbox_isa, extra_flags, callback.func, callback.user_data, callback.flags);
     return ppb_nacl_get_nexe_fd(instance, pexe_url, abi_version, opt_level, last_modified, etag,
                                 has_no_store_header, sandbox_isa, extra_flags, is_hit, nexe_handle,
                                 callback);
@@ -276,7 +288,7 @@ TRACE_WRAPPER
 void
 trace_ppb_nacl_report_translation_finished(PP_Instance instance, PP_Bool success)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d, success=%u\n", __func__+6, instance, success);
     return ppb_nacl_report_translation_finished(instance, success);
 }
 
@@ -285,7 +297,7 @@ PP_FileHandle
 trace_ppb_nacl_open_nacl_executable(PP_Instance instance, const char *file_url,
                                     uint64_t *file_token_lo, uint64_t *file_token_hi)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d, file_url=%s\n", __func__+6, instance, file_url);
     return ppb_nacl_open_nacl_executable(instance, file_url, file_token_lo, file_token_hi);
 }
 
@@ -295,7 +307,10 @@ trace_ppb_nacl_dispatch_event(PP_Instance instance, PP_NaClEventType event_type,
                               const char *resource_url, PP_Bool length_is_computable,
                               uint64_t loaded_bytes, uint64_t total_bytes)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d, event_type=%s, resource_url=%s, "
+               "length_is_computable=%u, loaded_bytes=%lu, total_bytes=%lu\n", __func__+6,
+               instance, reverse_nacl_event_type(event_type), resource_url, length_is_computable,
+               loaded_bytes, total_bytes);
     return ppb_nacl_dispatch_event(instance, event_type, resource_url, length_is_computable,
                                    loaded_bytes, total_bytes);
 }
@@ -304,7 +319,12 @@ TRACE_WRAPPER
 void
 trace_ppb_nacl_set_read_only_property(PP_Instance instance, struct PP_Var key, struct PP_Var value)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    gchar *s_key = trace_var_as_string(key);
+    gchar *s_value = trace_var_as_string(value);
+    trace_info("[PPB] {zilch} %s instance=%d, key=%s, value=%s\n", __func__+6, instance,
+               s_key, s_value);
+    g_free(s_key);
+    g_free(s_value);
     return ppb_nacl_set_read_only_property(instance, key, value);
 }
 
@@ -313,7 +333,8 @@ void
 trace_ppb_nacl_report_load_error(PP_Instance instance, PP_NaClError error,
                                  const char *error_message, const char *console_message)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d, error=%s, error_message=%s, console_message=%s\n",
+               __func__+6, instance, reverse_nacl_error(error), error_message, console_message);
     return ppb_nacl_report_load_error(instance, error, error_message, console_message);
 }
 
@@ -321,7 +342,7 @@ TRACE_WRAPPER
 void
 trace_ppb_nacl_instance_created(PP_Instance instance)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d\n", __func__+6, instance);
     return ppb_nacl_instance_created(instance);
 }
 
@@ -329,7 +350,7 @@ TRACE_WRAPPER
 void
 trace_ppb_nacl_instance_destroyed(PP_Instance instance)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d\n", __func__+6, instance);
     return ppb_nacl_instance_destroyed(instance);
 }
 
@@ -337,7 +358,7 @@ TRACE_WRAPPER
 PP_Bool
 trace_ppb_nacl_nacl_debug_enabled_for_url(const char *alleged_nmf_url)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s alleged_nmf_url=%s\n", __func__+6, alleged_nmf_url);
     return ppb_nacl_nacl_debug_enabled_for_url(alleged_nmf_url);
 }
 
@@ -353,7 +374,9 @@ TRACE_WRAPPER
 PP_UrlSchemeType
 trace_ppb_nacl_get_url_scheme(struct PP_Var url)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    gchar *s_url = trace_var_as_string(url);
+    trace_info("[PPB] {zilch} %s url=%s\n", __func__+6, s_url);
+    g_free(s_url);
     return ppb_nacl_get_url_scheme(url);
 }
 
@@ -361,7 +384,7 @@ TRACE_WRAPPER
 void
 trace_ppb_nacl_log_to_console(PP_Instance instance, const char *message)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d, message=%s\n", __func__+6, instance, message);
     return ppb_nacl_log_to_console(instance, message);
 }
 
@@ -369,7 +392,7 @@ TRACE_WRAPPER
 PP_Bool
 trace_ppb_nacl_get_nexe_error_reported(PP_Instance instance)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d\n", __func__+6, instance);
     return ppb_nacl_get_nexe_error_reported(instance);
 }
 
@@ -377,7 +400,8 @@ TRACE_WRAPPER
 void
 trace_ppb_nacl_set_nexe_error_reported(PP_Instance instance, PP_Bool error_reported)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d, error_reported=%u\n", __func__+6,
+               instance, error_reported);
     return ppb_nacl_set_nexe_error_reported(instance, error_reported);
 }
 
@@ -385,7 +409,7 @@ TRACE_WRAPPER
 PP_NaClReadyState
 trace_ppb_nacl_get_nacl_ready_state(PP_Instance instance)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d\n", __func__+6, instance);
     return ppb_nacl_get_nacl_ready_state(instance);
 }
 
@@ -393,7 +417,8 @@ TRACE_WRAPPER
 void
 trace_ppb_nacl_set_nacl_ready_state(PP_Instance instance, PP_NaClReadyState ready_state)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d, ready_state=%s\n", __func__+6,
+               instance, reverse_nacl_ready_state(ready_state));
     return ppb_nacl_set_nacl_ready_state(instance, ready_state);
 }
 
@@ -401,7 +426,7 @@ TRACE_WRAPPER
 PP_Bool
 trace_ppb_nacl_get_is_installed(PP_Instance instance)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d\n", __func__+6, instance);
     return ppb_nacl_get_is_installed(instance);
 }
 
@@ -409,7 +434,8 @@ TRACE_WRAPPER
 void
 trace_ppb_nacl_set_is_installed(PP_Instance instance, PP_Bool is_installed)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d, is_installed=%u\n", __func__+6,
+               instance, is_installed);
     return ppb_nacl_set_is_installed(instance, is_installed);
 }
 
