a339f67b3b433dc3df1848abdd950a7a90c1d44d
Rinat <ibragimovrinat@mail.ru>
2014-07-06 01:44:24 +0400

    ppb_file_io: add parameter tracing

--- src/ppb_file_io.c
+++ src/ppb_file_io.c
@@ -113,7 +113,8 @@ int32_t
 trace_ppb_file_io_request_os_file_handle(PP_Resource file_io, PP_FileHandle *handle,
                                          struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s file_io=%d, callback={.func=%p, .user_data=%p, .flags=%u}\n",
+               __func__+6, file_io, callback.func, callback.user_data, callback.flags);
     return ppb_file_io_request_os_file_handle(file_io, handle, callback);
 }
 
@@ -121,7 +122,7 @@ TRACE_WRAPPER
 PP_Resource
 trace_ppb_file_io_create(PP_Instance instance)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s instance=%d\n", __func__+6, instance);
     return ppb_file_io_create(instance);
 }
 
@@ -129,7 +130,7 @@ TRACE_WRAPPER
 PP_Bool
 trace_ppb_file_io_is_file_io(PP_Resource resource)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s resource=%d\n", __func__+6, resource);
     return ppb_file_io_is_file_io(resource);
 }
 
@@ -138,7 +139,9 @@ int32_t
 trace_ppb_file_io_open(PP_Resource file_io, PP_Resource file_ref, int32_t open_flags,
                        struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s file_io=%d, file_ref=%d, open_flags=%u, callback={.func=%p, "
+               ".user_data=%p, .flags=%u}\n", __func__+6, file_io, file_ref, open_flags,
+               callback.func, callback.user_data, callback.flags);
     return ppb_file_io_open(file_io, file_ref, open_flags, callback);
 }
 
@@ -147,7 +150,8 @@ int32_t
 trace_ppb_file_io_query(PP_Resource file_io, struct PP_FileInfo *info,
                         struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s file_io=%d, callback={.func=%p, .user_data=%p, .flags=%u}\n",
+               __func__+6, file_io, callback.func, callback.user_data, callback.flags);
     return ppb_file_io_query(file_io, info, callback);
 }
 
@@ -156,7 +160,10 @@ int32_t
 trace_ppb_file_io_touch(PP_Resource file_io, PP_Time last_access_time, PP_Time last_modified_time,
                         struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s file_io=%d, last_access_time=%f, last_modified_time=%f, "
+               "callback={.func=%p, .user_data=%p, .flags=%u}\n", __func__+6, file_io,
+               last_access_time, last_modified_time, callback.func, callback.user_data,
+               callback.flags);
     return ppb_file_io_touch(file_io, last_access_time, last_modified_time, callback);
 }
 
@@ -165,7 +172,9 @@ int32_t
 trace_ppb_file_io_read(PP_Resource file_io, int64_t offset, char *buffer, int32_t bytes_to_read,
                        struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s file_io=%d, offset=%ld, bytes_to_read=%d, callback={.func=%p, "
+               ".user_data=%p, .flags=%u}\n", __func__+6, file_io, offset, bytes_to_read,
+               callback.func, callback.user_data, callback.flags);
     return ppb_file_io_read(file_io, offset, buffer, bytes_to_read, callback);
 }
 
@@ -174,7 +183,9 @@ int32_t
 trace_ppb_file_io_write(PP_Resource file_io, int64_t offset, const char *buffer,
                         int32_t bytes_to_write, struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s file_io=%d, offset=%ld, bytes_to_write=%d, callback={.func=%p, "
+               ".user_data=%p, .flags=%u}\n", __func__+6, file_io, offset, bytes_to_write,
+               callback.func, callback.user_data, callback.flags);
     return ppb_file_io_write(file_io, offset, buffer, bytes_to_write, callback);
 }
 
@@ -183,7 +194,9 @@ int32_t
 trace_ppb_file_io_set_length(PP_Resource file_io, int64_t length,
                              struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s file_io=%d, length=%ld, callback={.func=%p, .user_data=%p, "
+               ".flags=%u}\n", __func__+6, file_io, length, callback.func, callback.user_data,
+               callback.flags);
     return ppb_file_io_set_length(file_io, length, callback);
 }
 
@@ -191,7 +204,8 @@ TRACE_WRAPPER
 int32_t
 trace_ppb_file_io_flush(PP_Resource file_io, struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s file_io=%d, callback={.func=%p, .user_data=%p, .flags=%u}\n",
+               __func__+6, file_io, callback.func, callback.user_data, callback.flags);
     return ppb_file_io_flush(file_io, callback);
 }
 
@@ -199,7 +213,7 @@ TRACE_WRAPPER
 void
 trace_ppb_file_io_close(PP_Resource file_io)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s file_io=%d\n", __func__+6, file_io);
     return ppb_file_io_close(file_io);
 }
 
@@ -208,7 +222,9 @@ int32_t
 trace_ppb_file_io_read_to_array(PP_Resource file_io, int64_t offset, int32_t max_read_length,
                                 struct PP_ArrayOutput *output, struct PP_CompletionCallback callback)
 {
-    trace_info("[PPB] {zilch} %s\n", __func__+6);
+    trace_info("[PPB] {zilch} %s file_io=%d, offset=%ld, max_read_length=%d, callback={.func=%p, "
+               ".user_data=%p, .flags=%u}\n", __func__+6, file_io, offset, max_read_length,
+               callback.func, callback.user_data, callback.flags);
     return ppb_file_io_read_to_array(file_io, offset, max_read_length, output, callback);
 }
 
