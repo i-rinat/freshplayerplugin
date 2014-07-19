e3b3abff4e9a9b05937570d9d5dfbad18942a98f
Rinat <ibragimovrinat@mail.ru>
2014-07-19 16:20:09 +0400

    ppb_graphics2d: don't wait for _call_invalidaterect

--- src/ppb_graphics2d.c
+++ src/ppb_graphics2d.c
@@ -158,23 +158,15 @@ ppb_graphics2d_replace_contents(PP_Resource graphics_2d, PP_Resource image_data)
     pp_resource_release(graphics_2d);
 }
 
-struct call_invalidaterect_param_s {
-    pthread_barrier_t       barrier;
-    struct pp_graphics2d_s *g2d;
-};
-
 static
 void
 _call_invalidaterect(void *param)
 {
-    struct call_invalidaterect_param_s *p = param;
-    NPP npp = p->g2d->instance->npp;
-    NPRect npr = {.top = 0, .left = 0, .bottom = p->g2d->height, .right = p->g2d->width};
+    struct pp_instance_s *pp_i = param;
+    NPRect npr = {.top = 0, .left = 0, .bottom = pp_i->height, .right = pp_i->width};
 
-    npn.invalidaterect(npp, &npr);
-    npn.forceredraw(npp);
-
-    pthread_barrier_wait(&p->barrier);
+    npn.invalidaterect(pp_i->npp, &npr);
+    npn.forceredraw(pp_i->npp);
 }
 
 int32_t
@@ -291,13 +283,8 @@ ppb_graphics2d_flush(PP_Resource graphics_2d, struct PP_CompletionCallback callb
         XFlush(pp_i->dpy);
         pthread_mutex_unlock(&pp_i->lock);
     } else {
-        struct call_invalidaterect_param_s p;
         pthread_mutex_unlock(&pp_i->lock);
-        p.g2d = g2d;
-        pthread_barrier_init(&p.barrier, NULL, 2);
-        npn.pluginthreadasynccall(pp_i->npp, _call_invalidaterect, &p);
-        pthread_barrier_wait(&p.barrier);
-        pthread_barrier_destroy(&p.barrier);
+        npn.pluginthreadasynccall(pp_i->npp, _call_invalidaterect, pp_i);
     }
 
     if (callback.func)
