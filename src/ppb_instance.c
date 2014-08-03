98ac33e78cb39e612d39775af147626371ded5ba
Rinat <ibragimovrinat@mail.ru>
2014-08-03 15:24:49 +0400

    ppb_instance_private_get_window_object: keep reference to np_window_obj

--- src/ppb_instance.c
+++ src/ppb_instance.c
@@ -114,6 +114,7 @@ ppb_instance_private_get_window_object(PP_Instance instance)
         return PP_MakeUndefined();
     }
 
+    npn.retainobject(pp_i->np_window_obj); // TODO: call on browser thread?
     return ppb_var_create_object(instance, &n2p_proxy_class, pp_i->np_window_obj);
 }
 
