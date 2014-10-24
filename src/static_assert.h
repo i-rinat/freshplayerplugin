eb366834de23c3db518035fe8292306efe878cd2
Rinat <ibragimovrinat@mail.ru>
2014-10-25 01:41:53 +0400

    add static_assert.h

---
+++ src/static_assert.h
@@ -0,0 +1,10 @@
+#ifndef FPP__STATIC_ASSERT_H
+#define FPP__STATIC_ASSERT_H
+
+
+#define STATIC_ASSERT_HELPER1(expr, msg)    typedef char static_assertion_##msg[(!!(expr))*2 - 1]
+#define STATIC_ASSERT_HELPER2(expr, msg)    STATIC_ASSERT_HELPER1(expr, msg)
+#define STATIC_ASSERT(expr)                 STATIC_ASSERT_HELPER2(expr, __LINE__)
+
+
+#endif // FPP__STATIC_ASSERT_H
