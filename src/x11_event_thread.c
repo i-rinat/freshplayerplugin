40272e0d7a29d3a0d3952a789ff5c5aa46874b20
Rinat Ibragimov <ibragimovrinat@mail.ru>
2015-05-16 14:11:08 +0300

    x11et: drop unused X11 Atom

--- src/x11_event_thread.c
+++ src/x11_event_thread.c
@@ -42,7 +42,6 @@ static pthread_mutex_t    lock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_t          thread;
 static uint32_t           thread_started = 0;
 static pthread_barrier_t  task_pass_barrier;
-static Atom               freshwrapper_x11et_cmd_atom;
 static Atom               xembed_atom;
 static Atom               xembed_info_atom;
 static int                task_pipe[2];
@@ -319,7 +318,7 @@ void
 x11et_start_thread(void)
 {
     dpy = XOpenDisplay(NULL);
-    freshwrapper_x11et_cmd_atom = XInternAtom(dpy, "FRESHWRAPPER_X11ET_CMD", False);
+
     xembed_atom =                 XInternAtom(dpy, "_XEMBED", False);
     xembed_info_atom =            XInternAtom(dpy, "_XEMBED_INFO", False);
 
