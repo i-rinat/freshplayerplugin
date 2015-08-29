655c2ba64df96d68ff376920b2c5cf39828817ec
Rinat Ibragimov <ibragimovrinat@mail.ru>
2015-08-30 02:00:54 +0300

    tests: add test for ppb_net_address_private

---
+++ tests/test_ppb_net_address.c
@@ -0,0 +1,191 @@
+#undef NDEBUG
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <src/ppb_net_address.c>
+#include "src/ppb_var.h"
+
+
+static
+void
+test_ppb_net_address_private(void)
+{
+    printf("%s\n", __func__);
+
+    printf("  ipv4, compose/decompose\n");
+    {
+        const uint8_t ip[4] = {192, 168, 1, 2};
+        const uint16_t port = 1234;
+        uint8_t ip_out[sizeof(ip)] = {};
+        struct PP_NetAddress_Private addr;
+
+        ppb_net_address_private_create_from_ipv4_address(ip, port, &addr);
+
+        ppb_net_address_private_get_address(&addr, ip_out, sizeof(ip_out));
+        assert(memcmp(ip, ip_out, sizeof(ip)) == 0);
+
+        assert(ppb_net_address_private_get_port(&addr) == port);
+        assert(ppb_net_address_private_get_family(&addr) == PP_NETADDRESSFAMILY_PRIVATE_IPV4);
+        assert(ppb_net_address_private_get_scope_id(&addr) == 0);
+    }
+
+    printf("  ipv4, equality\n");
+    {
+        const uint8_t ip[4] = {192, 168, 1, 2};
+        const uint16_t port1 = 1234;
+        const uint16_t port2 = 5678;
+
+        struct PP_NetAddress_Private addr1;
+        struct PP_NetAddress_Private addr2;
+
+        ppb_net_address_private_create_from_ipv4_address(ip, port1, &addr1);
+        ppb_net_address_private_create_from_ipv4_address(ip, port2, &addr2);
+
+        // different ports, hence are different
+        assert(ppb_net_address_private_are_equal(&addr1, &addr2) == PP_FALSE);
+        assert(ppb_net_address_private_are_hosts_equal(&addr1, &addr2) == PP_TRUE);
+
+        ppb_net_address_private_create_from_ipv4_address(ip, port2, &addr1);
+        ppb_net_address_private_create_from_ipv4_address(ip, port2, &addr2);
+
+        // should be equal
+        assert(ppb_net_address_private_are_equal(&addr1, &addr2) == PP_TRUE);
+        assert(ppb_net_address_private_are_hosts_equal(&addr1, &addr2) == PP_TRUE);
+    }
+
+    printf("  ipv4, replace port\n");
+    {
+        const uint8_t ip[4] = {192, 168, 1, 2};
+        const uint16_t port1 = 1234;
+        const uint16_t port2 = 1234;
+
+        struct PP_NetAddress_Private addr1;
+        struct PP_NetAddress_Private addr2;
+
+        ppb_net_address_private_create_from_ipv4_address(ip, port1, &addr1);
+        assert(ppb_net_address_private_get_port(&addr1) == port1);
+
+        ppb_net_address_private_replace_port(&addr1, port2, &addr2);
+        assert(ppb_net_address_private_get_port(&addr2) == port2);
+    }
+
+    printf("  ipv4, describe\n");
+    {
+        const uint8_t ip[4] = {192, 168, 1, 2};
+        const uint16_t port = 1234;
+
+        struct PP_NetAddress_Private addr;
+        struct PP_Var s;
+
+        ppb_net_address_private_create_from_ipv4_address(ip, port, &addr);
+        s = ppb_net_address_private_describe(0, &addr, PP_TRUE);
+        assert(strcmp(ppb_var_var_to_utf8(s, NULL), "192.168.1.2:1234") == 0);
+        ppb_var_release(s);
+
+        s = ppb_net_address_private_describe(0, &addr, PP_FALSE);
+        assert(strcmp(ppb_var_var_to_utf8(s, NULL), "192.168.1.2") == 0);
+        ppb_var_release(s);
+    }
+
+    printf("  ipv6, compose/decompose\n");
+    {
+        const uint8_t ip[16] = { 0x01, 0x23, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde,
+                                 0xef, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77 };
+        const uint16_t port = 3333;
+        const uint32_t scope_id = 555;
+
+        struct PP_NetAddress_Private addr;
+        uint8_t ip_out[sizeof(ip)] = {};
+
+        ppb_net_address_private_create_from_ipv6_address(ip, scope_id, port, &addr);
+
+        ppb_net_address_private_get_address(&addr, ip_out, sizeof(ip_out));
+        assert(memcmp(ip, ip_out, sizeof(ip)) == 0);
+
+        assert(ppb_net_address_private_get_port(&addr) == port);
+        assert(ppb_net_address_private_get_family(&addr) == PP_NETADDRESSFAMILY_PRIVATE_IPV6);
+        assert(ppb_net_address_private_get_scope_id(&addr) == scope_id);
+    }
+
+    printf("  ipv6, equality\n");
+    {
+        const uint8_t ip[16] = { 0x01, 0x23, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde,
+                                 0xef, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77 };
+        const uint16_t port1 = 3333;
+        const uint16_t port2 = 5555;
+
+        struct PP_NetAddress_Private addr1;
+        struct PP_NetAddress_Private addr2;
+
+        // different ports
+        ppb_net_address_private_create_from_ipv6_address(ip, 0, port1, &addr1);
+        ppb_net_address_private_create_from_ipv6_address(ip, 0, port2, &addr2);
+
+        // different, but hosts are the same
+        assert(ppb_net_address_private_are_equal(&addr1, &addr2) == PP_FALSE);
+        assert(ppb_net_address_private_are_hosts_equal(&addr1, &addr2) == PP_TRUE);
+
+        // same ports
+        ppb_net_address_private_create_from_ipv6_address(ip, 0, port2, &addr1);
+        ppb_net_address_private_create_from_ipv6_address(ip, 0, port2, &addr2);
+
+        // should be equal
+        assert(ppb_net_address_private_are_equal(&addr1, &addr2) == PP_TRUE);
+        assert(ppb_net_address_private_are_hosts_equal(&addr1, &addr2) == PP_TRUE);
+    }
+
+    printf("  ipv6, replace port\n");
+    {
+        const uint8_t ip[16] = { 0x01, 0x23, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde,
+                                 0xef, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77 };
+        const uint16_t port1 = 1234;
+        const uint16_t port2 = 1234;
+
+        struct PP_NetAddress_Private addr1;
+        struct PP_NetAddress_Private addr2;
+
+        ppb_net_address_private_create_from_ipv6_address(ip, 0, port1, &addr1);
+        assert(ppb_net_address_private_get_port(&addr1) == port1);
+
+        ppb_net_address_private_replace_port(&addr1, port2, &addr2);
+        assert(ppb_net_address_private_get_port(&addr2) == port2);
+    }
+
+    printf("  ipv6, describe\n");
+    {
+        const uint8_t ip[16] = { 0x01, 0x23, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde,
+                                 0xef, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77 };
+        const uint16_t port = 1234;
+
+        struct PP_NetAddress_Private addr;
+        struct PP_Var s;
+
+        ppb_net_address_private_create_from_ipv6_address(ip, 0, port, &addr);
+        s = ppb_net_address_private_describe(0, &addr, PP_TRUE);
+        assert(strcmp(ppb_var_var_to_utf8(s, NULL),
+                      "[123:3456:789a:bcde:ef11:2233:4455:6677]:1234") == 0);
+        ppb_var_release(s);
+
+        s = ppb_net_address_private_describe(0, &addr, PP_FALSE);
+        assert(strcmp(ppb_var_var_to_utf8(s, NULL),
+                      "123:3456:789a:bcde:ef11:2233:4455:6677") == 0);
+        ppb_var_release(s);
+    }
+}
+
+static
+void
+test_ppb_net_address(void)
+{
+    printf("%s\n", __func__);
+}
+
+int
+main(void)
+{
+    test_ppb_net_address_private();
+    test_ppb_net_address();
+
+    printf("pass\n");
+    return 0;
+}
