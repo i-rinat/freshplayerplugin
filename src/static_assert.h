5d668da925759a3e76919e270d69b6641a02d71d
Rinat Ibragimov <ibragimovrinat@mail.ru>
2015-08-30 01:56:57 +0300

    static_assert: make possible to use in local functions

--- src/static_assert.h
+++ src/static_assert.h
@@ -26,7 +26,8 @@
 #define FPP_STATIC_ASSERT_H
 
 
-#define STATIC_ASSERT_HELPER1(expr, msg)    typedef char static_assertion_##msg[(!!(expr))*2 - 1]
+#define STATIC_ASSERT_HELPER1(expr, msg)    typedef char static_assertion_##msg[(!!(expr))*2 - 1] \
+                                            __attribute__((used))
 #define STATIC_ASSERT_HELPER2(expr, msg)    STATIC_ASSERT_HELPER1(expr, msg)
 #define STATIC_ASSERT(expr)                 STATIC_ASSERT_HELPER2(expr, __LINE__)
 
