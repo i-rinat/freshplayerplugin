680e165ca61b72114764c65ed60b173f3a49e6b8
Rinat Ibragimov <ibragimovrinat@mail.ru>
2015-08-30 16:14:56 +0300

    tests: add tests for ppb_net_address

--- tests/test_ppb_net_address.c
+++ tests/test_ppb_net_address.c
@@ -4,6 +4,11 @@
 #include <string.h>
 #include <src/ppb_net_address.c>
 #include "src/ppb_var.h"
+#include "common.h"
+#include "src/ppb_core.h"
+
+
+static PP_Instance instance;
 
 
 static
@@ -231,13 +236,60 @@ void
 test_ppb_net_address(void)
 {
     printf("%s\n", __func__);
+
+    printf("  ipv4, compose/decompose\n");
+    {
+        const struct PP_NetAddress_IPv4 ipv4 = { .addr = {192, 168, 1, 2}, .port = 3456 };
+        struct PP_NetAddress_IPv4 ipv4_a = {};
+        struct PP_NetAddress_IPv6 ipv6 = {};
+
+        PP_Resource addr = ppb_net_address_create_from_ipv4_address(instance, &ipv4);
+        assert(ppb_net_address_is_net_address(addr) == PP_TRUE);
+        assert(ppb_net_address_get_family(addr) == PP_NETADDRESS_FAMILY_IPV4);
+        assert(ppb_net_address_describe_as_ipv6_address(addr, &ipv6) == PP_FALSE);
+        assert(ppb_net_address_describe_as_ipv4_address(addr, &ipv4_a) == PP_TRUE);
+
+        assert(memcmp(&ipv4, &ipv4_a, sizeof(ipv4_a)) == 0);
+
+        struct PP_Var s = ppb_net_address_describe_as_string(addr, PP_TRUE);
+        assert(strcmp(ppb_var_var_to_utf8(s, NULL), "192.168.1.2:3456") == 0);
+        ppb_var_release(s);
+
+        ppb_core_release_resource(addr);
+    }
+
+    printf("  ipv6, compose/decompose\n");
+    {
+        const struct PP_NetAddress_IPv6 ipv6 = { .addr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                                                          12, 13, 14, 15, 16}, .port = 3456 };
+        struct PP_NetAddress_IPv6 ipv6_a = {};
+        struct PP_NetAddress_IPv4 ipv4 = {};
+
+        PP_Resource addr = ppb_net_address_create_from_ipv6_address(instance, &ipv6);
+        assert(ppb_net_address_is_net_address(addr) == PP_TRUE);
+        assert(ppb_net_address_get_family(addr) == PP_NETADDRESS_FAMILY_IPV6);
+        assert(ppb_net_address_describe_as_ipv4_address(addr, &ipv4) == PP_FALSE);
+
+        assert(ppb_net_address_describe_as_ipv6_address(addr, &ipv6_a) == PP_TRUE);
+        assert(memcmp(&ipv6, &ipv6_a, sizeof(ipv6_a)) == 0);
+
+        struct PP_Var s = ppb_net_address_describe_as_string(addr, PP_TRUE);
+        assert(strcmp(ppb_var_var_to_utf8(s, NULL), "[102:304:506:708:90a:b0c:d0e:f10]:3456") == 0);
+        ppb_var_release(s);
+
+        ppb_core_release_resource(addr);
+    }
 }
 
 int
 main(void)
 {
+    instance = create_instance();
+
     test_ppb_net_address_private();
     test_ppb_net_address();
+
+    destroy_instance(instance);
 
     printf("pass\n");
     return 0;
